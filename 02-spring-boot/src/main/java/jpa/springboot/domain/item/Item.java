a768f3b0abaef5a6e5e6f53067080ba2594c56be
rtg <rtg1539@naver.com>
2024-05-08 19:01:10 +0900

    상품 도메인

--- 02-spring-boot/src/main/java/jpa/springboot/domain/item/Item.java
+++ 02-spring-boot/src/main/java/jpa/springboot/domain/item/Item.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.ManyToMany;
 import jpa.springboot.domain.Category;
+import jpa.springboot.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,10 +27,23 @@ public class Item {
     @Column(name = "item_id")
     private Long id;
 
-    @ManyToMany(mappedBy = "items")
-    private List<Category> categories = new ArrayList<>();
-
     private String name;
     private int price;
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
