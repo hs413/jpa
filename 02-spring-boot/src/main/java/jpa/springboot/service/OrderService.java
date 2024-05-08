cf3fb8aec502bc348ed7e191eb29d87d6d6ff88b
rtg <rtg1539@naver.com>
2024-05-08 21:21:48 +0900

    주문 도메인

---
+++ 02-spring-boot/src/main/java/jpa/springboot/service/OrderService.java
@@ -0,0 +1,62 @@
+package jpa.springboot.service;
+
+import jpa.springboot.domain.Delivery;
+import jpa.springboot.domain.DeliveryStatus;
+import jpa.springboot.domain.Member;
+import jpa.springboot.domain.Order;
+import jpa.springboot.domain.OrderItem;
+import jpa.springboot.domain.item.Item;
+import jpa.springboot.repository.ItemRepository;
+import jpa.springboot.repository.MemberRepository;
+import jpa.springboot.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final MemberRepository memberRepository;
+    private final OrderRepository orderRepository;
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+        // 엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        // 배송정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+        delivery.setStatus(DeliveryStatus.READY);
+
+        // 주문상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        // 주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        // 주문 저장
+        orderRepository.save(order);
+        return order.getId();
+    }
+
+    /** 주문 취소 **/
+    @Transactional
+    public void cancelOrder(Long orderId) {
+        // 주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+
+        // 주문 취소
+        order.cancel();
+    }
+
+    /** 주문 검색 **/
+    /*
+     * public List<Order> findOrders(OrderSearch ordersearch) {
+     *   return orderRepository.findAll(orderSearch);
+     * }*/
+}
