dad45830ddd6178659e5f5f2a0ec26ce3fd968b0
rtg <rtg1539@naver.com>
2024-05-12 11:02:02 +0900

    순수 JPA repository

--- 03-spring-data-jpa/src/main/java/jpa/datajpa/repository/MemberJpaRepository.java
+++ 03-spring-data-jpa/src/main/java/jpa/datajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,10 @@ import jakarta.persistence.PersistenceContext;
 import jpa.datajpa.entity.Member;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
 /**
  * JPA 사용
  * */
@@ -17,6 +21,25 @@ public class MemberJpaRepository {
         em.persist(member);
         return member;
     }
+
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class).getSingleResult();
+    }
+
 
     public Member find(Long id) {
         return em.find(Member.class, id);
