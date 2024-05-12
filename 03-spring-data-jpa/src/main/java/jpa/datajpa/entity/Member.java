1484a9e654e6076f5b74c04f1ffe9795ce0f2d1a
rtg <rtg1539@naver.com>
2024-05-12 11:34:55 +0900

    쿼리 메서드 기능, @Query

--- 03-spring-data-jpa/src/main/java/jpa/datajpa/entity/Member.java
+++ 03-spring-data-jpa/src/main/java/jpa/datajpa/entity/Member.java
@@ -9,6 +9,10 @@ import lombok.*;
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(of = {"id", "username", "age"}) // 내부 필드만 사용, 참조 X
+//@NamedQuery(
+//        name="Member.findByUsername",
+//        query="select m from Member m where m.username = :username"
+//)
 public class Member {
     @Id @GeneratedValue
     @Column(name = "member_id")
--- 03-spring-data-jpa/src/test/java/jpa/datajpa/repository/MemberJpaRepositoryTest.java
+++ 03-spring-data-jpa/src/test/java/jpa/datajpa/repository/MemberJpaRepositoryTest.java
@@ -2,7 +2,6 @@ package jpa.datajpa.repository;
 
 import jakarta.transaction.Transactional;
 import jpa.datajpa.entity.Member;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,21 +10,18 @@ import org.springframework.test.annotation.Rollback;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
 @Rollback(value = false)
 class MemberJpaRepositoryTest {
-    @Autowired MemberJpaRepository memberJpaRepository;
-    @Autowired
-    private MemberRepository memberRepository;
+    @Autowired MemberJpaRepository memberRepository;
 
     @Test
     public void testMember() {
         Member member = new Member("memberA");
-        Member savedMember = memberJpaRepository.save(member);
-        Member findMember = memberJpaRepository.find(savedMember.getId());
+        Member savedMember = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedMember.getId());
 
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
@@ -40,24 +36,37 @@ class MemberJpaRepositoryTest {
         memberRepository.save(member2);
 
         // 조회
-        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
-        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
         assertThat(findMember1).isEqualTo(member1);
         assertThat(findMember2).isEqualTo(member2);
 
         // 리스트 조회
-        List<Member> members = memberJpaRepository.findAll();
+        List<Member> members = memberRepository.findAll();
         assertThat(members).hasSize(2);
 
         // 카운트
-        long count = memberJpaRepository.count();
+        long count = memberRepository.count();
         assertThat(count).isEqualTo(2);
 
         // 삭제
         memberRepository.delete(member1);
         memberRepository.delete(member2);
 
-        long deleteCount = memberJpaRepository.count();
+        long deleteCount = memberRepository.count();
         assertThat(deleteCount).isEqualTo(0);
+    }
+
+    @Test
+    public void findByUsernameAndAgeGreaterThan() {
+        Member member1 = new Member("member1", 10, null);
+        Member member2 = new Member("member1", 20, null);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> result = memberRepository.findByUsernameAndAgeGreaterThan("member1", 15);
+        assertThat(result.get(0).getUsername()).isEqualTo("member1");
+        assertThat(result.get(0).getAge()).isEqualTo(20);
+        assertThat(result.size()).isEqualTo(1);
     }
 }
