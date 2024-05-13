8c482f5733f5ad50d05128fa222e1da91d322d57
rtg <rtg1539@naver.com>
2024-05-13 23:09:40 +0900

    기본 문법 테스트 분할 설명 수정

--- 04-jpa-querydsl/src/test/java/jpa/querydsl/basic/JoinTest.java
+++ 04-jpa-querydsl/src/test/java/jpa/querydsl/basic/JoinTest.java
@@ -11,11 +11,13 @@ import static jpa.querydsl.entity.QMember.member;
 import static jpa.querydsl.entity.QTeam.team;
 import static org.assertj.core.api.Assertions.assertThat;
 
+
+/**
+ * 조인 테스트
+ * */
 public class JoinTest extends BasicTestInit{
-    /**
-     * 조인 - 기본 조인
-     * */
     @Test
+    @DisplayName("기본 조인")
     public void join() {
         List<Member> result = queryFactory
                 .selectFrom(member)
@@ -28,10 +30,8 @@ public class JoinTest extends BasicTestInit{
                 .containsExactly("member1", "member2");
     }
 
-    /**
-     * 조인 - 세타 조인 (연관관계가 없는 필드로 조인)
-     * */
     @Test
+    @DisplayName("세타 조인 (연관관계가 없는 필드로 조인)")
     public void theta_join() throws Exception {
         em.persist(new Member("TeamA"));
         em.persist(new Member("TeamB"));
@@ -45,12 +45,9 @@ public class JoinTest extends BasicTestInit{
                 .containsExactly("TeamA", "TeamB");
     }
 
-    /**
-     * 조인 - on
-     * - 내부조인이면 where 절로 해결, 외부조인이 필요한 경우에만 사용
-     * */
+    // ON 테스트 - 내부조인이면 where 절로 해결, 외부조인이 필요한 경우에만 사용
     @Test
-    @DisplayName("회원과 팀을 조인하면서, 팀 이름이 TeamA인 팀만 조인 하고 회원은 모두 조회한다.")
+    @DisplayName("ON 테스트 - 회원과 팀을 조인하면서, 팀 이름이 TeamA인 팀만 조인")
     public void join_on_filtering() throws Exception {
         List<Tuple> result = queryFactory
                 .select(member, team)
@@ -63,7 +60,7 @@ public class JoinTest extends BasicTestInit{
     }
 
     @Test
-    @DisplayName("연관관계가 없는 엔터티 외부 조인으로 회원의 이름과 팀 이름이 같은 회원을 조인한다.")
+    @DisplayName("ON 테스트 - 연관관계가 없는 엔터티 외부 조인, 회원의 이름과 팀 이름이 같은 팀 조인")
     void joinOnNoRelation(){
         //given
         em.persist(new Member("TeamA"));
@@ -81,9 +78,6 @@ public class JoinTest extends BasicTestInit{
         }
     }
 
-    /**
-     * 조인 - 페치 조인
-     * */
     @Test
     @DisplayName("패치조인 미사용")
     void fetchJoinNo(){
