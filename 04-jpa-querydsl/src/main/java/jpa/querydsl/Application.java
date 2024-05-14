512c6b7934d6674ffd122b406ddb44b1094c1cf3
rtg <rtg1539@naver.com>
2024-05-14 12:29:26 +0900

    JPA, QueryDsl 비교 테스트

--- 04-jpa-querydsl/src/main/java/jpa/querydsl/Application.java
+++ 04-jpa-querydsl/src/main/java/jpa/querydsl/Application.java
@@ -1,7 +1,10 @@
 package jpa.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +13,8 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
 }
