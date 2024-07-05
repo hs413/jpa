5a75817754fbb537f6aa3602ae9d73cc0b5c8cb8
rtg <rtg1539@naver.com>
2024-07-05 20:27:12 +0900

    [JPA 프로그래밍] 4장 엔티티 매핑 - 다양한 매핑 사용

---
+++ jpa/04-엔티티_매핑/start/src/main/java/jpabook/start/Member.java
@@ -0,0 +1,54 @@
+package jpabook.start;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name="MEMBER")
+public class Member {
+
+    @Id
+    @Column(name = "ID")
+    private String id;
+
+    @Column(name= "NAME")
+    private String username;
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
