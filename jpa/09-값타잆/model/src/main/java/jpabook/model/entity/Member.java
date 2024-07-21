c45ed2412025d6d0f544b3ae533dbdcc4366d4f1
rtg <rtg1539@naver.com>
2024-07-21 21:21:59 +0900

    [JPA 프로그래밍] 9장 값 타입 예제 프로젝트 세팅

---
+++ jpa/09-값타잆/model/src/main/java/jpabook/model/entity/Member.java
@@ -0,0 +1,27 @@
+package jpabook.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Member extends BaseEntity {
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    private String name;
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    @OneToMany
+    private List<Order> orders = new ArrayList<>();
+}
