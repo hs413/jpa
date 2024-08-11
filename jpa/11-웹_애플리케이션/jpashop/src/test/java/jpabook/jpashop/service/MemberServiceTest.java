a4502c78eb31ab9ccf4ef043397287a1938c55f5
rtg <rtg1539@naver.com>
2024-08-11 18:00:29 +0900

    [JPA 프로그래밍] 11장 웹 애플리케이션 - 회원 repository, service test

---
+++ jpa/11-웹_애플리케이션/jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,52 @@
+package jpabook.jpashop.service;
+
+import jakarta.transaction.Transactional;
+import jpabook.jpashop.entity.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception {
+        // Given
+        Member member = new Member();
+        member.setName("kim");
+
+        // When
+        Long saveId = memberService.join(member);
+
+        // Then
+        assertThat(member).isEqualTo(memberRepository.findOne(saveId));
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        // Given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+
+        // When
+        memberRepository.save(member1);
+
+        // then
+        assertThatThrownBy(() -> memberService.join(member2))
+                .isInstanceOf(IllegalStateException.class);
+    }
+}
