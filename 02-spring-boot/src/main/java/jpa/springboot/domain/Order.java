e59cf88760fe1dc4cf03778c991109302fca485b
rtg <rtg1539@naver.com>
2025-06-08 17:34:38 +0900

    엔티티 클래스

--- 02-spring-boot/src/main/java/jpa/springboot/domain/Order.java
+++ 02-spring-boot/src/main/java/jpa/springboot/domain/Order.java
@@ -1,7 +1,9 @@
 package jpa.springboot.domain;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -25,19 +27,34 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
     private LocalDateTime orderDate;
 
     private OrderStatus status;
+
+    //==연관관계 메소드==//
+    public void setMember (Member member){
+        this.member= member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem (OrderItem orderItem){
+        orderItems.add(orderItem);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 
 }
