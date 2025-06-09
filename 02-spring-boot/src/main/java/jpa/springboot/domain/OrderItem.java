8138801ee8be8edea1bc36a55efc86e7cdd11cbe
rtg <rtg1539@naver.com>
2025-06-09 21:21:48 +0900

    주문 도메인

--- 02-spring-boot/src/main/java/jpa/springboot/domain/OrderItem.java
+++ 02-spring-boot/src/main/java/jpa/springboot/domain/OrderItem.java
@@ -8,11 +8,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jpa.springboot.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
     @Id
     @GeneratedValue
@@ -27,7 +30,28 @@ public class OrderItem {
     @JoinColumn(name = "order_id")
     private Order order;
 
-    private Long orderPrice;
+    private int orderPrice;
+    private int count;
 
-    private Long count;
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비지니스 로직==//
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+    /** 주문상품 전체 가격 조회 */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
