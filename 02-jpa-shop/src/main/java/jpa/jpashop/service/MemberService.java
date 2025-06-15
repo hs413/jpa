8c3cf9c6eb1d5987e638cf87dd964cc67822fd72
rtg <rtg1539@naver.com>
2025-06-15 10:16:11 +0900

    회원 서비스

---
+++ 02-jpa-shop/src/main/java/jpa/jpashop/service/MemberService.java
@@ -0,0 +1,40 @@
+package jpa.jpashop.service;
+
+import jpa.jpashop.domain.Member;
+import jpa.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)  // 조회 시 성능 최적화
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    @Transactional // readOnly = false, 우선 적용 됨
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다");
+        }
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+
+}
