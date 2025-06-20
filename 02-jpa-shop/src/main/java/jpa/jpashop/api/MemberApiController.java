d1909adeb7b27889787d672e3d532832eb11ed29
rtg <rtg1539@naver.com>
2025-06-20 17:00:26 +0900

    회원 수정 API

--- 02-jpa-shop/src/main/java/jpa/jpashop/api/MemberApiController.java
+++ 02-jpa-shop/src/main/java/jpa/jpashop/api/MemberApiController.java
@@ -3,8 +3,11 @@ package jpa.jpashop.api;
 import jakarta.validation.Valid;
 import jpa.jpashop.domain.Member;
 import jpa.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,6 +44,26 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
+
+    /**
+     * command와 query는 분리하는 것이 좋음 -> 유지보수성 증가
+     * - memberService.update(id, request.getName());  // command
+     * - Member findMember = memberService.findOne(id); // query
+     * */
+    @PatchMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+        @PathVariable("id") Long id,
+        @RequestBody @Valid UpdateMemberRequest request) {
+
+        memberService.update(id, request.getName());  // command
+        Member findMember = memberService.findOne(id); // query
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+
+
+
+
     @Data
     static class CreateMemberRequest {
         private String name;
@@ -52,5 +75,17 @@ public class MemberApiController {
         public CreateMemberResponse(Long id) {
             this.id = id;
         }
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
     }
 }
