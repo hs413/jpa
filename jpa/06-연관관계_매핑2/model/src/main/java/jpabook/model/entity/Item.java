446dcc4b24e9c34b747c3e545353f5333726b430
rtg <rtg1539@naver.com>
2025-07-13 15:39:55 +0900

    [JPA 프로그래밍] 6장 연관관계 매핑2 - 예제 프로젝트 세팅

---
+++ jpa/06-연관관계_매핑2/model/src/main/java/jpabook/model/entity/Item.java
@@ -0,0 +1,22 @@
+package jpabook.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+public class Item {
+    @Id
+    @GeneratedValue
+    @Column(name = "ITEM_ID")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+}
