2f53e27dfb04f87b63cbedce71d107ff9a0f8940
rtg <rtg1539@naver.com>
2025-07-17 15:04:16 +0900

    [JPA 프로그래밍] 7장 고급매핑 - 예제 세팅

---
+++ jpa/07-고급매핑/model/src/main/java/jpabook/model/entity/Item.java
@@ -0,0 +1,25 @@
+package jpabook.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+public class Item {
+    @Id
+    @GeneratedValue
+    @Column(name = "ITEM_ID")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
+}
