fb3626368e2706b55c120e1ccef06bfe381a03c6
rtg <rtg1539@naver.com>
2025-07-22 21:21:59 +0900

    [JPA 프로그래밍] 9장 값 타입 예제 프로젝트 세팅

---
+++ jpa/09-값타잆/model/src/main/java/jpabook/model/entity/item/Item.java
@@ -0,0 +1,28 @@
+package jpabook.model.entity.item;
+
+import jakarta.persistence.*;
+import jpabook.model.entity.Category;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "DTYPE")
+@Setter
+@Getter
+public abstract class Item {
+    @Id
+    @GeneratedValue
+    @Column(name = "ITEM_ID")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
+}
