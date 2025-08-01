ef380efc40dc2ae7f9e0ef8dda4265907eb0a262
rtg <rtg1539@naver.com>
2025-08-01 22:08:22 +0900

    [JPA 프로그래밍] 11장 웹 애플리케이션 - 주문 기능 웹 계층

--- jpa/11-웹_애플리케이션/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ jpa/11-웹_애플리케이션/jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -26,4 +26,23 @@ public class ItemService {
         return itemRepository.findOne(id);
     }
 
+    /**
+     * == 변경 감지를 사용한 업데이트 ==
+     * 영속성 엔티티로 조회한 뒤 변경 감지 기능을 사용 한다.
+     * 변경 감지는 커밋 시점 실행 된다.
+     */
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId);
+
+//        findItem.setName(name);
+//        findItem.setPrice(price);
+//        findItem.setStockQuantity(stockQuantity);
+
+        /**
+         * change 메서드를 사용 하도록 refactoring
+         * setter는 가급적 사용 X
+         * */
+        findItem.change(name, price, stockQuantity);
+    }
 }
